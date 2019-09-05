58069e838941237f000f0725bd50687e3efa1e54
pscsi <pscsi@w209061K.pscnet.com.tw>
2019-09-05 10:29:13 +0800

    dine

--- hello.go
+++ hello.go
@@ -6,6 +6,13 @@ func main() {
 	fmt.Println(Hello("world"))
 }
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
