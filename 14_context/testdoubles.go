fff7d5ba10db41ac8c71d4b99aa4646569522bbc
peien <peien0312@gmail.com>
2019-09-13 16:54:31 +0800

    store

---
+++ 14_context/testdoubles.go
@@ -0,0 +1,21 @@
+package store
+
+import (
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+	t         *testing.T
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
