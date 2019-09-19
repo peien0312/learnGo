0c8ebfeb41d68ac619e90c7ee5e8e4552fd0c746
Peien <peien.wang@uni-psg.com>
2019-09-19 17:05:34 +0800

    aaa

--- 16_math/clockface/clock_test.go
+++ 16_math/clockface/clock_test.go
@@ -1,56 +1,58 @@
 package clockface
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+// func TestSecondHandAtMidnight(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := Point{X: 150, Y: 150 - 90}
-	got := SecondHand(tm)
+// 	want := Point{X: 150, Y: 150 - 90}
+// 	got := SecondHand(tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+// func TestSecondHandAt30Seconds(t *testing.T) {
+// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-	want := Point{X: 150, Y: 150 + 90}
-	got := SecondHand(tm)
+// 	want := Point{X: 150, Y: 150 + 90}
+// 	got := SecondHand(tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("Got %v, wanted %v", got, want)
+// 	}
+// }
 
-func TestSecondsInRadians(t *testing.T) {
+// func TestSecondsInRadians(t *testing.T) {
 
-	cases := []struct {
-		time  time.Time
-		angle float64
-	}{
-		{simpleTime(0, 0, 30), math.Pi},
-		{simpleTime(0, 0, 0), 0},
-		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
-		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
-	}
+// 	cases := []struct {
+// 		time  time.Time
+// 		angle float64
+// 	}{
+// 		{simpleTime(0, 0, 30), math.Pi},
+// 		{simpleTime(0, 0, 0), 0},
+// 		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+// 		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+// 	}
 
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			got := secondsInRadians(c.time)
+// 	for _, c := range cases {
+// 		t.Run(testName(c.time), func(t *testing.T) {
+// 			got := secondsInRadians(c.time)
 
-			if got != c.angle {
-				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
-			}
-		})
-	}
+// 			if got != c.angle {
+// 				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+// 			}
+// 		})
+// 	}
 
-}
+// }
 func simpleTime(hours, minutes, seconds int) time.Time {
 	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
@@ -85,4 +87,53 @@ func roughlyEqualFloat64(a, b float64) bool {
 
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+
+	SVGWriter(&b, tm)
+
+	svg := SVG{}
+
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000"
+	y2 := "60.000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+
+}
+
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
